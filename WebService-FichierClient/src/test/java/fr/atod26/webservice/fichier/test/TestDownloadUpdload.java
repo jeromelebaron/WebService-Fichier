1d3e13c11015d542325b9d89ef94f3fefd2465bd
Jerdopler <lebaronjerome@free.fr>
2016-04-21 14:05:25 +0200

    MAJ du test

--- WebService-FichierClient/src/test/java/fr/atod26/webservice/fichier/test/TestDownloadUpdload.java
+++ WebService-FichierClient/src/test/java/fr/atod26/webservice/fichier/test/TestDownloadUpdload.java
@@ -1,10 +1,12 @@
 package fr.atod26.webservice.fichier.test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.log4j.Logger;
 
 import fr.atod26.webservice.fichier.lebaronjerome.GestionImage;
@@ -25,10 +27,9 @@ public class TestDownloadUpdload {
 		proxy = service.getGestionImageImplPort();
 		byte[] imageTrouvee = proxy.recupererImage("Lighthouse.jpg");
 		try {
-			FileOutputStream fileOutputStream = new FileOutputStream(new File(
-					Thread.currentThread().getContextClassLoader()
-							.getResource("./").getPath()
-							+ "Lighthouse.jpg"));
+			FileOutputStream fileOutputStream = new FileOutputStream(new File(Thread.currentThread()
+					.getContextClassLoader().getResource("./").getPath()
+					+ "Lighthouse.jpg"));
 			fileOutputStream.write(imageTrouvee);
 			fileOutputStream.close();
 			log.info("Image enregistrée");
@@ -38,5 +39,21 @@ public class TestDownloadUpdload {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		File apple = new File(Thread.currentThread().getContextClassLoader().getResource("./").getPath() + "apple.png");
+		byte[] appleByte = new byte[(int) apple.length()];
+		FileInputStream fileInputStream;
+		try {
+			fileInputStream = new FileInputStream(apple);
+			fileInputStream.read(appleByte);
+			fileInputStream.close();
+			String base64File = Base64.encodeBase64String(appleByte);
+			log.info(proxy.envoyerImage("appleLogo.png", base64File));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
